e1dcdffa16f105c2900403a613ac7035fea6cd31
Dorian Tsai <dorian.tsai@gmail.com>
2021-03-05 15:59:19 +1100

    comments updated

--- serrated-tussock-detector/SerratedTussockDataset.py
+++ serrated-tussock-detector/SerratedTussockDataset.py
@@ -4,41 +4,54 @@ import os
 import numpy as np
 import torch
 import torch.utils.data
-from PIL import Image
-# import pandas as pd
 import json
 import matplotlib.pyplot as plt
-# from skimage import transform as sktrans
-from torchvision.transforms import functional as tvtransfunc
 import torchvision.transforms as T
 import random
+
+# from skimage import transform as sktrans
+from PIL import Image
+from torchvision.transforms import functional as tvtransfunc
 from torchvision.datasets.video_utils import VideoClips
 
 
-""" Serrated tussock dataset """
 class SerratedTussockDataset(object):
+    """ Serrated tussock dataset """
+
 
     def __init__(self, root_dir, json_file, transforms):
-        # self.weed_frame = pd.read_csv(csv_file)
+        """
+        initialise the dataset
+        annotations - json file of annotations of a prescribed format
+        root_dir - root directory of dataset, under which are Images and Annotations folder
+        transforms - list of transforms randomly applied to dataset for training
+        """
+
         annotations = json.load(open(os.path.join(root_dir, json_file)))
         self.annotations = list(annotations.values())
         self.root_dir = root_dir
         self.transforms = transforms
 
+
     def __getitem__(self, idx):
+        """
+        given an index, return the corresponding image and sample from the dataset
+        converts images and corresponding sample to tensors
+        """
+
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
         # get image
-        # img_name = os.path.join(self.root_dir, self.annotations.iloc[idx, 0])
         img_name = os.path.join(self.root_dir, 'Images', self.annotations[idx]['filename'])
         image =  Image.open(img_name).convert("RGB")
-        # image = plt.imread(img_name)
 
         # number of bboxes
         nobj = len(self.annotations[idx]['regions'])
 
         # get bbox
+        # bounding box is read in a xmin, ymin, width and height
+        # bounding box is saved as xmin, ymin, xmax, ymax
         boxes = []
         for i in range(nobj):
             xmin = self.annotations[idx]['regions'][str(i)]['shape_attributes']['x']
@@ -48,22 +61,24 @@ class SerratedTussockDataset(object):
             xmax = xmin + width
             ymax = ymin + height
             boxes.append([xmin, ymin, xmax, ymax])
-
         boxes = torch.as_tensor(boxes, dtype=torch.float64)
+
+        # compute area
         if len(boxes) == 0:
             # no boxes
             area = 0
-        else: 
+        else:
             area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-
         area = torch.as_tensor(area, dtype=torch.float64)
 
-        # only one class, so:
+        # only one class + background:
         labels = torch.ones((nobj,), dtype=torch.int64)
 
         # TODO iscrowd?
         iscrowd = torch.zeros((nobj,), dtype=torch.int64)
 
+        # image_id is the index of the image in the folder
+        # TODO should test this
         image_id = torch.tensor([idx], dtype=torch.int64)
 
         sample = {}
@@ -72,26 +87,33 @@ class SerratedTussockDataset(object):
         sample['image_id'] = image_id
         sample['area'] = area
         sample['iscrowd'] = iscrowd
-        # sample['image_name'] = img_name
-        # sample['image'] = image
 
+        # apply transforms to image and sample
         if self.transforms:
-            # https://discuss.pytorch.org/t/t-compose-typeerror-call-takes-2-positional-arguments-but-3-were-given/62529/2
             image, sample = self.transforms(image, sample)
-            # image = sample['image']
 
         return image, sample
 
 
     def __len__(self):
+        """
+        return the number of images in the entire dataset
+        """
         return len(self.annotations)
 
 
 class Compose(object):
+    """ Compose for set of transforms """
+
+
     def __init__(self, transforms):
         self.transforms = transforms
 
+
     def __call__(self, image, target):
+        # NOTE this is done because the built-in PyTorch Compose transforms function
+        # only accepts a single (image/tensor) input. To operate on both the image
+        #  as well as the sample/target, we need a custom Compose transform function
         for t in self.transforms:
             image, target = t(image, target)
         return image, target
@@ -106,13 +128,9 @@ class Rescale(object):
 
     def __call__(self, image, sample=None):
         # image = sample['image']
-        
+
         # handle the aspect ratio
         h, w = image.size[:2]
-
-        # import code
-        # code.interact(local=dict(globals(), **locals()))
-
         if isinstance(self.output_size, int):
             if h > w:
                 new_h, new_w = self.output_size * h / w, self.output_size
@@ -120,29 +138,22 @@ class Rescale(object):
                 new_h, new_w = self.output_size, self.output_size * w / h
         else:
             new_h, new_w = self.output_size
-        
+
         new_h, new_w = int(new_h), int(new_w)
 
         # do the transform
-        # sktrans works on a numpy array
-        # img = sktrans.resize(image, (new_h, new_w))
         img = T.Resize((new_w, new_h))(image)
 
-        # import code
-        # code.interact(local=dict(globals(), **locals()))
-
-        # TODO apply transform to bbox as well
+        # apply transform to bbox as well
         if sample is not None:
-            bbox = sample["boxes"]  # [xmin ymin xmax ymax]
-            # bbox[:, [0, 2]] = width - bbox[:, [2, 0]]
-
             xChange = float(new_w) / float(w)
             yChange = float(new_h) / float(h)
+            bbox = sample["boxes"]  # [xmin ymin xmax ymax]
 
-            if len(bbox) == 0:
+            # if len(bbox) == 0:
                 # do nothing, since there are no bboxes
-                print('rescale transform: no boxes here')
-            else:
+                # print('rescale transform: no boxes here')
+            if len(bbox) > 0:
                 bbox[:, 0] = bbox[:, 0] * yChange
                 bbox[:, 1] = bbox[:, 1] * xChange
                 bbox[:, 2] = bbox[:, 2] * yChange
@@ -150,35 +161,40 @@ class Rescale(object):
                 sample["boxes"] = np.float64(bbox)
 
             return img, sample
-        
         else:
             return img
 
 
 class ToTensor(object):
     """ convert ndarray to sample in Tensors """
-    
-    def __call__(self, image, sample):
-        # image = sample['image']
 
-        # image = image.transpose((2, 0, 1))
-        # image = torch.from_numpy(image)
+    def __call__(self, image, sample):
+        """ convert image and sample to tensors """
+
+        # convert image
         image = tvtransfunc.to_tensor(image)
+
+        # convert samples
         boxes = sample['boxes']
         if not torch.is_tensor(boxes):
             boxes = torch.from_numpy(boxes)
         sample['boxes'] = boxes
 
-        # sample['image'] = image
         return image, sample
 
 
 class RandomHorizontalFlip(object):
     """ Random horozintal flip """
+
     def __init__(self, prob):
+        """ probability of a horizontal image flip """
+
         self.prob = prob
 
+
     def __call__(self, image, sample):
+        """ apply horizontal image flip to image and sample """
+
         if random.random() < self.prob:
             w, h = image.size[:2]
             # flip image
@@ -186,13 +202,10 @@ class RandomHorizontalFlip(object):
 
             # flip bbox
             bbox = sample['boxes']
-            # bbox[:, [0, 2]] = w - bbox[:, [0, 2]]
-            # import code
-            # code.interact(local=dict(globals(), **locals())) 
 
-            if len(bbox) == 0:
-                print('horizontal flip transform: no boxes here')
-            else:
+            # bounding box is saved as xmin, ymin, xmax, ymax
+            # only changing xmin and xmax
+            if len(bbox) > 0:
                 bbox[:, [0, 2]] = w - bbox[:, [2, 0]]  # note the indices switch (must flip the box as well!)
                 sample['boxes'] = bbox
 
@@ -203,11 +216,13 @@ class Blur(object):
     """ Gaussian blur images """
 
     def __init__(self, kernel_size=3, sigma=(0.1, 2.0)):
+        """ kernel size and standard deviation (sigma) of Gaussian blur """
         self.kernel_size = kernel_size
         self.sigma = sigma
 
-    def __call__(self, image, sample):
 
+    def __call__(self, image, sample):
+        """ apply blur to image """
         image = tvtransfunc.gaussian_blur(image, self.kernel_size, self.sigma)
 
         return image, sample
