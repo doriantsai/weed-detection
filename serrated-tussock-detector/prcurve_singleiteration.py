e41e982e45a2fba3569feca2eae9480be7afa089
Dorian Tsai <dorian.tsai@gmail.com>
2021-04-16 13:13:35 +1000

    full pr-curve extension, extrapolation and interpolation, ap score, f1score

--- serrated-tussock-detector/prcurve_singleiteration.py
+++ serrated-tussock-detector/prcurve_singleiteration.py
@@ -15,10 +15,11 @@ import matplotlib.pyplot as plt
 import os
 import time
 
+from scipy.interpolate import interp1d
 from train import build_model
 from PIL import Image
-from inference import show_groundtruth_and_prediction_bbox, cv_imshow
-from get_prediction import get_prediction_image
+from inference import show_groundtruth_and_prediction_bbox, cv_imshow, infer_dataset
+from get_prediction import get_prediction_image, threshold_predictions
 
 
 def compute_iou_bbox(boxA, boxB):
@@ -190,3 +191,317 @@ def compute_outcome_image(pred,
                 'gt_iou_all': gt_iou_all,
                 'cost': cost}
     return outcomes
+
+
+
+def compute_single_pr_over_dataset(model,
+                                   dataset,
+                                   predictions,
+                                   save_name,
+                                   NMS_IOU_THRESH,
+                                   MODEL_REFJECT_CONF_THRESH,
+                                   DECISION_IOU_THRESH,
+                                   DECISION_CONF_THRESH,
+                                   imsave=False):
+    model.eval()
+    model.to(device)
+
+    tp_sum = 0
+    fp_sum = 0
+    tn_sum = 0
+    fn_sum = 0
+    gt_sum = 0
+    dt_sum = 0
+    idx = 0
+    dataset_outcomes = []
+    for image, sample in dataset:
+
+        image_id = sample['image_id'].item()
+        img_name = 'st' + str(image_id).zfill(3)
+
+
+        # get predictions
+        pred = predictions[idx]
+        pred = threshold_predictions(pred, MODEL_REFJECT_CONF_THRESH)
+        # now, reject all those below MODEL_REJECT_CONF_THRESH
+
+        # pred, keep = get_prediction_image(model,
+        #                                   image,
+        #                                   MODEL_REFJECT_CONF_THRESH, # set low to allow FNs
+        #                                   NMS_IOU_THRESH,
+        #                                   device,
+        #                                   class_names)
+
+        outcomes = compute_outcome_image(pred,
+                                         sample,
+                                         DECISION_IOU_THRESH,
+                                         DECISION_CONF_THRESH)
+
+        # show for given image:
+        if imsave:
+            img_out = show_groundtruth_and_prediction_bbox(image,
+                                                        sample,
+                                                        pred,
+                                                        outcomes=outcomes)
+
+            imgw = cv.cvtColor(img_out, cv.COLOR_RGB2BGR)
+            save_folder_name = os.path.join('output',
+                                            save_name,
+                                            'outcomes',
+                                            'conf_thresh_' + str(DECISION_CONF_THRESH))
+            if not os.path.exists(save_folder_name):
+                os.mkdir(save_folder_name)
+            save_img_name = os.path.join(save_folder_name, img_name + '_outcome.png')
+            cv.imwrite(save_img_name, imgw)
+            # winname = 'outcome: ' + img_name
+            # cv_imshow(img_out, winname, 1000)
+
+        tp = outcomes['tp']
+        fp = outcomes['fp']
+        fn_gt = outcomes['fn_gt']
+        fn_dt = outcomes['fn_dt']
+        fn = np.concatenate((fn_gt, fn_dt), axis=0)
+        gt = len(outcomes['gt_match'])
+        dt = len(outcomes['dt_match'])
+
+        tp_sum += np.sum(tp)
+        fp_sum += np.sum(fp)
+        fn_sum += np.sum(fn)
+        gt_sum += np.sum(gt)
+        dt_sum += np.sum(dt)
+        # end per image outcome calculations
+
+        dataset_outcomes.append(outcomes)
+        idx += 1
+
+    print('tp sum: ', tp_sum)
+    print('fp sum: ', fp_sum)
+    print('fn sum: ', fn_sum)
+    print('dt sum: ', dt_sum)
+    print('tp + fp = ', tp_sum + fp_sum)
+    print('gt sum: ', gt_sum)
+    print('tp + fn = ', tp_sum + fn_sum)
+
+    prec = tp_sum / (tp_sum + fp_sum)
+    rec = tp_sum / (tp_sum + fn_sum)
+
+    print('precision = {}'.format(prec))
+    print('recall = {}'.format(rec))
+
+    f1score = 2 * (prec * rec) / (prec + rec)
+    print('f1 score = {}'.format(f1score))
+    # 1 is good, 0 is bad
+
+    return dataset_outcomes, prec, rec, f1score
+
+
+if __name__ == "__main__":
+
+    # load model
+    # load datasets
+    # infer on relevant dataset
+    # iterate over decision threshold
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    model = build_model(num_classes=2)
+    class_names = ['__background__', 'tussock']
+
+    save_name = 'Tussock_v0_8'
+    model_save_path = os.path.join('output', save_name, save_name + '.pth')
+    model.load_state_dict(torch.load(model_save_path))
+
+    # dataset location
+    data_save_path = os.path.join('.', 'output', save_name, save_name + '.pkl')
+    with open(data_save_path, 'rb') as f:
+        dataset_tform_test = pickle.load(f)
+        dataset_tform_train = pickle.load(f)
+        dataset_train = pickle.load(f)
+        dataset_val = pickle.load(f)
+        dataset_test = pickle.load(f)
+        dataloader_test = pickle.load(f)
+        dataloader_train = pickle.load(f)
+        dataloader_val = pickle.load(f)
+        hp = pickle.load(f)
+
+    # infer on dataset with 0-decision threshold
+    out_raw, predictions = infer_dataset(model,
+                                        dataset_test,
+                                        confidence_threshold=0,
+                                        iou_threshold=0.5,
+                                        save_folder_name=save_name,
+                                        device=device,
+                                        class_names=class_names,
+                                        output_folder='prcurve_predictions',
+                                        dataset=dataset_test.dataset,
+                                        imshow=False,
+                                        img_name_suffix='_prcurve_0')
+
+    # compute AP
+    # choose best F1 score?
+    # report 95% recall and 95% precision values
+
+    NMS_IOU_THRESH = 0.5
+    DECISION_IOU_THRESH = 0.5
+    # DECISION_CONF_THRESH = 0.5
+    DECISION_CONF_THRESH = np.linspace(0.95, 0.05, num=11, endpoint=True)
+    DECISION_CONF_THRESH = np.array(DECISION_CONF_THRESH, ndmin=1)
+
+
+    prec = []
+    rec = []
+    f1score = []
+    start_time = time.time()
+    for c, conf in enumerate(DECISION_CONF_THRESH):
+        print('{}: outcome confidence threshold: {}'.format(c, conf))
+        dataset_outcomes, p, r, f1 = compute_single_pr_over_dataset(model,
+                                                                    dataset_test,
+                                                                    predictions,
+                                                                    save_name,
+                                                                    NMS_IOU_THRESH,
+                                                                    conf,
+                                                                    DECISION_IOU_THRESH,
+                                                                    conf,
+                                                                    imsave=False)
+
+        # single-line for copy/paste in terminal:
+        # d, p, r, f1 = compute_single_pr_over_dataset(model, dataset_test, predictions, NMS_IOU_THRESH, conf, DECISION_IOU_THRESH, conf, imsave=True)
+        prec.append(p)
+        rec.append(r)
+        f1score.append(f1)
+        # do we care about dataset_outcomes?
+
+    end_time = time.time()
+
+    sec = end_time - start_time
+    print('training time: {} sec'.format(sec))
+    print('training time: {} min'.format(sec / 60.0))
+    print('training time: {} hrs'.format(sec / 3600.0))
+
+    # plot raw PR curve
+    rec = np.array(rec)
+    prec = np.array(prec)
+    fig, ax = plt.subplots()
+    ax.plot(rec, prec, marker='o', linestyle='dashed')
+    plt.xlabel('recall')
+    plt.ylabel('precision')
+    plt.title('precision-recall for varying confidence')
+    save_plot_name = os.path.join('output', save_name, save_name + '_test_pr.png')
+    plt.savefig(save_plot_name)
+    # plt.show()
+
+    # plot F1score
+    f1score = np.array(f1score)
+    fig, ax = plt.subplots()
+    ax.plot(rec, f1score, marker='o', linestyle='dashed')
+    plt.xlabel('recall')
+    plt.ylabel('f1 score')
+    plt.title('f1 score vs recall for varying confidence')
+    save_plot_name = os.path.join('output', save_name, save_name + '_test_f1r.png')
+    plt.savefig(save_plot_name)
+    # plt.show()
+
+    # smooth the PR curve: take the max precision values along the recall curve
+    # we do this by binning the recall values, and taking the max precision from
+    # each bin
+
+
+    # take max:
+    diff_thresh = 0.001
+    dif = np.diff(rec)
+    prec_new = [prec[0]]
+    for i, d in enumerate(dif):
+        if d < diff_thresh:
+            prec_new.append(prec_new[-1])
+        else:
+            prec_new.append(prec[i+1])
+    prec_new = np.array(prec_new)
+    print(prec_new)
+
+
+
+    fig, ax = plt.subplots()
+    ax.plot(rec, prec, marker='o', linestyle='dashed', label='original')
+    ax.plot(rec, prec_new, marker='x', color='red', linestyle='solid', label='max-binned')
+    plt.xlabel('recall')
+    plt.ylabel('precision')
+    plt.title('prec-rec, max-binned')
+    ax.legend()
+    save_plot_name = os.path.join('output', save_name, save_name + '_test_pr_smooth.png')
+    plt.savefig(save_plot_name)
+
+    # now, expand prec/rec values to extent of the whole precision/recall space:
+    rec_x = np.linspace(0, 1, num=101, endpoint=True)
+
+    # create prec_x by concatenating vectors first
+    prec_temp = []
+    rec_temp = []
+    for r in rec_x:
+        if r < rec[0]:
+            prec_temp.append(prec_new[0])
+            rec_temp.append(r)
+
+    for i, r in enumerate(rec):
+        prec_temp.append(prec_new[i])
+        rec_temp.append(r)
+
+    for r in rec_x:
+        if r >= rec[-1]:
+            prec_temp.append(0)
+            rec_temp.append(r)
+
+    prec_temp = np.array(prec_temp)
+    rec_temp = np.array(rec_temp)
+
+    # now interpolate:
+    # prec_x = np.interp(rec_x, rec_temp, prec_temp)
+    prec_interpolator = interp1d(rec_temp, prec_temp, kind='linear')
+    prec_x = prec_interpolator(rec_x)
+
+    fig, ax = plt.subplots()
+    ax.plot(rec_temp, prec_temp, color='blue', linestyle='dashed', label='combined')
+    ax.plot(rec, prec_new, marker='x', color='red', linestyle='solid', label='max-binned')
+    ax.plot(rec_x, prec_x, color='green', linestyle='dotted', label='interp')
+    plt.xlabel('recall')
+    plt.ylabel('precision')
+    plt.title('prec-rec, interpolated')
+    ax.legend()
+    save_plot_name = os.path.join('output', save_name, save_name + '_test_pr_interp.png')
+    plt.savefig(save_plot_name)
+    # plt.show()
+
+    # make the "official" plot:
+    p_final = prec_x
+    r_final = rec_x
+
+    n = len(r_final) - 1
+    ap = np.sum( (r_final[1:n] - r_final[0:n-1]) * p_final[1:n] )
+
+    fig, ax = plt.subplots()
+    ax.plot(r_final, p_final)
+    plt.xlabel('recall')
+    plt.ylabel('precision')
+    plt.title('prec-rec curve, iou={}, ap = {:.2f}'.format(DECISION_IOU_THRESH, ap))
+    # ax.legend()
+    save_plot_name = os.path.join('output', save_name, save_name + '_test_pr_final.png')
+    plt.savefig(save_plot_name)
+    plt.show()
+
+    print('ap score: {:.5f}'.format(ap))
+    print('max f1 score: {:.5f}'.format(max(f1score)))
+
+    # save ap, f1score, precision, recall, etc
+    res = {'precision': p_final,
+           'recall': r_final,
+           'ap': ap,
+           'f1score': f1score}
+    save_file = os.path.join('output', save_name, save_name + '_prcurve.pkl')
+    with open(save_file, 'wb') as f:
+        pickle.dump(res, f)
+
+
+    # TODO
+    # 95% precision, what confidence value?
+    # 95% recall, what confidence value?
+    import code
+    code.interact(local=dict(globals(), **locals()))
