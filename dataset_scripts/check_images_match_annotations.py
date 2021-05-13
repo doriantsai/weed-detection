22c827017749048eac63adaf45f303c84cf88ec1
Dorian Tsai <dorian.tsai@gmail.com>
2021-05-13 21:31:27 +1000

    fixed install and imports so check_images_match_annotations.py works

--- dataset_scripts/check_images_match_annotations.py
+++ dataset_scripts/check_images_match_annotations.py
@@ -9,7 +9,7 @@ from weed_detection import PreProcessingToolbox
 
 pt = PreProcessingToolbox()
 
-root_dir = os.path.join('/home', 'dorian','Data','AOS_TussockDataset')
+root_dir = os.path.join('/home', 'dorian','Data','AOS_TussockDataset', 'Tussock_210325_G507_location1_positivetags')
 img_dir = os.path.join(root_dir, 'positive-tags')
 ann_file = os.path.join(root_dir, 'Annotations', 'Thursday_25-03-21_G507_location1_positive-tags_labels.json')
 
