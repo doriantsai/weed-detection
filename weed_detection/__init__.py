a4672e6f426640b3393dfb93d7a156e0f52d55e7
Dorian Tsai <dorian.tsai@gmail.com>
2021-07-20 16:36:45 +1000

    bootstrap_polygons in progress, needs image overlay to be improved, image_overlay in progress with binarize_image/finding contours. Seriously consider using MVT for these operations

--- weed_detection/__init__.py
+++ weed_detection/__init__.py
@@ -1,6 +1,6 @@
 from weed_detection.PreProcessingToolbox import PreProcessingToolbox
-from weed_detection.WeedDataset import *
 from weed_detection.WeedModel import *
+from weed_detection.WeedDataset import *
 from weed_detection.WeedDatasetPoly import *
 # from weed_detection.utils import *
 # from weed_detection.coco_eval import *
