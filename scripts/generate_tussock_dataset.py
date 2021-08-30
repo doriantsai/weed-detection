8354f6d0bcae05380cc2b414f11a7b724b2061d8
Dorian Tsai <dorian.tsai@gmail.com>
2021-08-30 15:43:38 +1000

    in progress splitting tussock into pos/neg images

--- scripts/generate_tussock_dataset.py
+++ scripts/generate_tussock_dataset.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 """
-script to go into weeds reference library setup,
+script to generate tussock dataset from weeds reference library setup,
 grab all image files
 grab all json files
 create one large merged json file for entire dataset
@@ -21,34 +21,31 @@ from weed_detection.PreProcessingToolbox import PreProcessingToolbox
 # output all photos into single folder via symlinks
 
 # dataset location/root_dir
-root_dir = os.path.join('/home/dorian/Data/AOS_TussockDataset/03_Tagged')
+dataset_name = '2021-03-25_MFS_Tussock'
+root_dir = os.path.join('/home/dorian/Data/AOS_TussockDataset', dataset_name)
+# img folder
+img_dir = os.path.join(root_dir, 'images')
+# annotation folder/file
+ann_dir = os.path.join(root_dir, 'metadata')
+ann_file = '2021-03-25_Tussock_MFS.json'
+ann_path = os.path.join(ann_dir, ann_file)
 
-# glob for all image files
-pattern_img = '*.png'
-# generally, find everything
-# glob_img = glob.glob(str(root_dir + '/*/*/images/' + pattern_img),
-#                      recursive=True)
-glob_img = glob.glob(str(root_dir + '/2021-03-25/*/images/' + pattern_img),
-                     recursive=True)
-glob_img2 = glob.glob(str(root_dir + '/2021-03-26/Location_1/images/' + pattern_img),
-                     recursive=True)
-glob_img.extend(glob_img2)
+# TODO want to filter the negative images vs positive images and put them into respective folders
+ann_dict = json.load(open(ann_path))
+ann_list = list(ann_dict.values())
 
-# glob for all annotation files
-pattern_ann = '*labels_polygons.json'
-glob_ann = glob.glob(str(root_dir + '/*/*/metadata/' + pattern_ann ),
-                     recursive=True)
-
-# check globs
-print(len(glob_img))
-print(len(glob_ann))
-print(glob_ann)
+# iterate through ann_list, if ann has regions, it is positive, else negative
+ann_pos = {}
+ann_neg = {}
+# TODO I am here
 
 # create folder for symlinks
-out_dir = os.path.join('/home/dorian/Data/AOS_TussockDataset/2021-03-25_MFS_Tussock')
-img_out_dir = os.path.join(out_dir, 'images_all')
+out_dir = os.path.join('/home/dorian/Data/AOS_TussockDataset/2021-03-25_Tussock_MFS')
+img_pos_out_dir = os.path.join(out_dir, 'images_pos')
+img_neg_out_dir = os.path.join(out_dir, 'images_neg')
 ann_out_dir = os.path.join(out_dir, 'metadata')
-os.makedirs(img_out_dir, exist_ok=True)
+os.makedirs(img_pos_out_dir, exist_ok=True)
+os.makedirs(img_neg_out_dir, exist_ok=True)
 os.makedirs(ann_out_dir, exist_ok=True)
 
 # create symlinks
@@ -70,7 +67,7 @@ print(f'num imgs in out_dir = {len(img_list)}')
 print('should be the same')
 
 # merge annotations into one:
-ann_out = '2021-03-25_MFS_Tussock.json'
+ann_out = '2021-03-25_Tussock_MFS.json'
 ann_out_path = os.path.join(ann_out_dir, ann_out)
 ProTool = PreProcessingToolbox()
 res = ProTool.combine_annotations(ann_files=glob_ann,
---
+++ scripts/sync_annotations_folders.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+
+"""
+sync annotations file for single image folder
+img_folder = folder of images that we want a matching annotations file
+ann_master_file = annotations file that does have all the annotations for all images in database
+ann_file_out = output annotations corresponding to images in img_folder
+"""
+
+import os
+import json
+from weed_detection.PreProcessingToolbox import PreProcessingToolbox
+
+# folder locations and file names
+# root_dir = os.path.join('/home/dorian/Data/AOS_TussockDataset/03_Tagged/2021-03-25/Location_1')
+# root_dir = os.path.join('/home/dorian/Data/AOS_TussockDataset/03_Tagged/2021-03-25/Location_2')
+root_dir = os.path.join('/home/dorian/Data/AOS_TussockDataset/03_Tagged/2021-03-26/Location_1')
+
+# folder containing all images to be used for development (testing/training/val) and deployment
+img_folder = os.path.join(root_dir, 'images')
+ann_dir = os.path.join(root_dir, 'metadata')
+print('img_dir = ' + img_folder)
+
+# annotation files Master (contains all images - we don't touch this file, just
+# use it as a reference/check)
+ann_master_dir = os.path.join('/home/dorian/Data/AOS_TussockDataset/Tussock_v4_poly/Annotations')
+ann_master_file = 'annotations_tussock_21032526_G507_master1.json'  # has the polygons
+ann_master_path = os.path.join(ann_master_dir, ann_master_file)
+print('ann_master_file = ' + ann_master_file)
+# annotation files out
+
+# ann_file_out = 'Thursday_25-03-21_G507_location1_positive-tags_labels_polygons.json'
+# ann_file_out = 'Thursday_25-03-21_G507_location2_positive-tags_labels_polygons.json'
+ann_file_out = 'Thursday_26-03-21_G507_location1_positive-tags_labels_polygons.json'
+ann_path_out = os.path.join(ann_dir, ann_file_out)
+print('ann_file_out = ' + ann_file_out)
+
+# use preprocessing toolbox
+ProTool = PreProcessingToolbox()
+res = ProTool.sync_annotations(img_folder, ann_master_path, ann_path_out)
+
+img_files = os.listdir(img_folder)
+n_imgs = len(img_files)
+print(f'img folder count = {n_imgs}')
+
+ann_dict = json.load(open(ann_path_out))
+n_ann_file = len(ann_dict)
+print(f'ann file count = {n_ann_file}')
+
+print('should be the same')
+
+import code
+code.interact(local=dict(globals(), **locals()))
