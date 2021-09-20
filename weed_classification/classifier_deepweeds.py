b56993515a30ae7e6edf87c6b960685db12e73b8
Dorian Tsai <dorian.tsai@gmail.com>
2021-09-20 16:25:59 +1000

    updated transforms, randompixelintensityscaling for deepweeds classifier

--- weed_classification/classifier_deepweeds.py
+++ weed_classification/classifier_deepweeds.py
@@ -24,6 +24,8 @@ import time
 import datetime
 import pickle
 
+import code
+
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils, models
@@ -31,169 +33,18 @@ from torchvision import transforms, utils, models
 # use tensorboard to see training in progress
 from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as F
+from deepweeds_dataset import DeepWeedsDataset, Rescale, RandomAffine, RandomColorJitter, RandomPixelIntensityScaling
+from deepweeds_dataset import RandomHorizontalFlip, RandomRotate, RandomResizedCrop, ToTensor, Compose
+from deepweeds_dataset import CLASSES, CLASS_NAMES
+
 
 np.random.seed(42)
 torch.manual_seed(42)
 
 
 
-########### classes #############
-CLASSES = (0, 1, 2, 3, 4, 5, 6, 7, 8)
-CLASS_NAMES = ('Chinee apple',
-               'Lantana',
-               'Parkinsonia',
-               'Parthenium',
-               'Prickly acacia',
-               'Rubber vine',
-               'Siam weed',
-               'Snake weed',
-               'Negative')
-CLASS_DICT = {i: CLASS_NAMES[i] for i in range(0, len(CLASSES))}
 
 
-class DeepWeedsDataset(Dataset):
-    """ Deep weeds dataset """
-
-    def __init__(self, csv_file, root_dir, transform=None):
-        # csv_file (string): Path to csv file with labels
-        # root_dir (string): Directory with all images
-        # transform (callable, opt): Transform to be applied to sample
-
-        self.weed_frame = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.weed_frame)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        img_name = os.path.join(self.root_dir, self.weed_frame.iloc[idx, 0])
-        image = plt.imread(img_name)
-        label = self.weed_frame.iloc[idx, 1]
-
-        # make sample into a dictionary
-        sample = {'image': image, 'label': label, 'image_id': idx}
-
-        # apply transform
-        if self.transform:
-            sample = self.transform(sample)
-
-        return sample
-
-
-# class Rescale(object):
-#     """ Rescale image to given size """
-
-#     def __init__(self, output_size):
-#         assert isinstance(output_size, (int, tuple))
-#         self.output_size = output_size
-
-#     def __call__(self, sample):
-#         # open up the dict
-#         image, label, id = sample['image'], sample['label'], sample['image_id']
-
-#         # handle the aspect ratio
-#         h, w = image.shape[:2]
-#         if isinstance(self.output_size, int):
-#             if h > w:
-#                 new_h, new_w = self.output_size * h / w, self.output_size
-#             else:
-#                 new_h, new_w = self.output_size, self.output_size * w / h
-#         else:
-#             new_h, new_w = self.output_size
-#         new_h, new_w = int(new_h), int(new_w)
-
-#         # do the transform
-#         img = transform.resize(image, (new_h, new_w))
-
-#         # return as a dictionary, as before
-#         return {'image': img, 'label': label, 'image_id': id}
-
-
-# class RandomCrop(object):
-#     """ Randomly crop image """
-
-#     def __init__(self, output_size):
-#         assert isinstance(output_size, (int, tuple))
-#         if isinstance(output_size, int):
-#             self.output_size = (output_size, output_size)
-#         else:
-#             assert len(output_size) == 2
-#             self.output_size = output_size
-
-#     def __call__(self, sample):
-#         # unpack the dictionary
-#         # get h,w
-#         # new h,w from output size
-#         # randomly crop from top/left
-#         # return image in sample
-#         image, label, id = sample['image'], sample['label'], sample['image_id']
-
-#         h, w = image.shape[:2]
-#         new_h, new_w = self.output_size
-
-#         top, left = np.random.randint(
-#             0, h - new_h), np.random.randint(0, w - new_w)
-#         image = image[top:top + new_h, left:left + new_w]
-
-#         return {'image': image, 'label': label, 'image_id': id}
-class RandomPixelIntensityScaling(object):
-    """ randomly scale pixel intensities in an image """
-
-    def __init__(self, prob, scale_min, scale_max):
-        self.prob = prob
-        self.scale_min = scale_min
-        self.scale_max = scale_max
-
-    def __call__(self, sample):
-        img = sample['image']
-
-        # TODO we have a PIL image
-        # if prob:
-        # do transform
-        # randomly choose a scale value between scale_min/max
-        # multiply entire image intensities by scale
-        # save to image sample/return image sample
-        # else
-        # just return normal
-
-class ToTensor(object):
-    """ convert ndarray to sample in Tensors """
-
-    def __call__(self, sample):
-        image, label, id = sample['image'], sample['label'], sample['image_id']
-
-        image = image.transpose((2, 0, 1))
-        return {'image': torch.from_numpy(image), 'label': label, 'image_id': id}
-
-# TODO rotation, then  scaled horz/vert, each colour channel shifted
-# can't we just call torchvision.transforms.RandomX?
-# class RandomHorizontalFlip(object):
-#     """ random horizontal flip """
-#     def __init__(self, prob):
-#         """ probability of horizontal image flip """
-#         self.prob = prob
-
-# class RandomRotate(object):
-#     """ randomly rotate image """
-#     def __init__(self, prob):
-#         self.prob = prob
-
-# class RandomColourJitter(object):
-#     """ randomly shift pixel intensity, colour"""
-#     # sere torchvision.transforms.ColorJitter(brightness)
-#     def __init__(self, prob):
-#         self.prob = prob
-
-# class RandomPerspective(object):
-#     """ randomly apply affine transformation for perspective shift """
-#     # see RandomAffine
-#     def ___init__(self, prob):
-#         self.prob = prob
-
-# TODO randomly apply transforms using "RandomApply"
 ############# functions #################
 
 
@@ -381,10 +232,11 @@ def train_model(model,
             running_loss = 0.0
             running_acc = 0.0
 
-            for step, sample_batch in enumerate(dataloader):
+            for sample_batch in dataloader:
                 # note: output from dataloader is all converted to tensors, I believe
-                imgs, lbls = sample_batch['image'].float(
-                ), sample_batch['label']
+                imgs, lbls = sample_batch['image'], sample_batch['label']
+
+
                 # We move our tensor to the GPU if available
                 if torch.cuda.is_available():
                     imgs = imgs.to(device)
@@ -565,14 +417,17 @@ if __name__ == "__main__":
     #                             RandomCrop(224),
     #                             ToTensor()
     #                             ])
-    tforms = transforms.Compose([
-        transforms.Resize(size=(256, 256)),
-        transforms.RandomRotation(degrees=(-360, 360)),
-        transforms.RandomResizedCrop(size=(256, 256), scale=(0.5, 1.0)),
-        transforms.ColorJitter(brightness=(0, 0.1), hue=(-0.01, 0.01)), # TODO unsure about these values
+
+    # TODO actually, I think I have to write classes for each, because they return sample, rather than just image...
+    tforms = Compose([
+        Rescale(256),
+        RandomRotate(360),
+        RandomResizedCrop(size=(224, 224), scale=(0.5, 1.0)),
+        RandomColorJitter(brightness=(0, 0.1), hue=(-0.01, 0.01)), # TODO unsure about these values
         RandomPixelIntensityScaling(),
-        transforms.RandomAffine(degrees=5, translate=(0.05, 0.05)),
-        transforms.RandomHorizontalFlip(p=0.5)
+        RandomAffine(degrees=5, translate=(0.05, 0.05)),
+        RandomHorizontalFlip(prob=0.5),
+        ToTensor()
     ])
     # full dataset
     full_dataset = DeepWeedsDataset(csv_file=labels_file,
@@ -703,8 +558,7 @@ if __name__ == "__main__":
 
             # define model (resnet 50, random initialised weights)
             model = models.resnet50(pretrained=False)
-            model.fc = nn.Linear(
-                in_features=2048, out_features=len(CLASSES), bias=True)
+            model.fc = nn.Linear(in_features=2048, out_features=len(CLASSES), bias=True)
 
             # select loss function and optimizer
             # TODO consider adding weights to classes (eg Negative class)
--- weed_classification/deepweeds_dataset.py
+++ weed_classification/deepweeds_dataset.py
@@ -9,6 +9,9 @@ from torchvision import transforms, utils, models
 from skimage import io, transform
 import pandas as pd
 import torch
+import random
+from PIL import Image
+from torchvision.transforms import functional as tvtransfunc
 
 
 class DeepWeedsDataset(Dataset):
@@ -30,7 +33,7 @@ class DeepWeedsDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         img_name = os.path.join(self.root_dir, self.weed_frame.iloc[idx, 0])
-        image = plt.imread(img_name)
+        image = Image.open(img_name)
         label = self.weed_frame.iloc[idx, 1]
 
         # make sample into a dictionary
@@ -55,7 +58,7 @@ class Rescale(object):
         image, label, id = sample['image'], sample['label'], sample['image_id']
 
         # handle the aspect ratio
-        h, w = image.shape[:2]
+        h, w = image.size[:2]
         if isinstance(self.output_size, int):
             if h > w:
                 new_h, new_w = self.output_size * h / w, self.output_size
@@ -66,37 +69,169 @@ class Rescale(object):
         new_h, new_w = int(new_h), int(new_w)
 
         # do the transform
-        img = transform.resize(image, (new_h, new_w))
+        img = transforms.Resize((new_w, new_h))(image)
 
         # return as a dictionary, as before
         return {'image': img, 'label': label, 'image_id': id}
 
 
-class RandomCrop(object):
+class RandomRotate(object):
+    """ randomly rotate object """
+
+
+    def __init__(self,  degrees):
+        self._degrees = degrees
+
+    def __call__(self, sample):
+        image, label, id = sample['image'], sample['label'], sample['image_id']
+        Rotation = transforms.RandomRotation(degrees=self._degrees)
+        img = Rotation(image)
+        return {'image': img, 'label': label, 'image_id': id}
+
+
+class RandomResizedCrop(object):
     """ Randomly crop image """
 
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        if isinstance(output_size, int):
-            self.output_size = (output_size, output_size)
-        else:
-            assert len(output_size) == 2
-            self.output_size = output_size
+    # def __init__(self, output_size):
+    #     assert isinstance(output_size, (int, tuple))
+    #     if isinstance(output_size, int):
+    #         self.output_size = (output_size, output_size)
+    #     else:
+    #         assert len(output_size) == 2
+    #         self.output_size = output_size
+    # def __call__(self, sample):
+    #     # unpack the dictionary
+    #     # get h,w
+    #     # new h,w from output size
+    #     # randomly crop from top/left
+    #     # return image in sample
+    #     image, label, id = sample['image'], sample['label'], sample['image_id']
+
+    #     h, w = image.shape[:2]
+    #     new_h, new_w = self.output_size
+
+    #     top, left = np.random.randint(0, h - new_h), np.random.randint(0, w - new_w)
+    #     image = image[top:top + new_h, left:left + new_w]
+
+    #     return {'image': image, 'label': label, 'image_id': id}
+
+    def __init__(self, size=(224, 224), scale=(0.5, 1.0)):
+
+        self._size = size
+        self._scale = scale
+
     def __call__(self, sample):
-        # unpack the dictionary
-        # get h,w
-        # new h,w from output size
-        # randomly crop from top/left
-        # return image in sample
         image, label, id = sample['image'], sample['label'], sample['image_id']
+        ResizeCrop = transforms.RandomResizedCrop(self._size, self._scale)
+        img = ResizeCrop(image)
+        return {'image': img, 'label': label, 'image_id': id}
 
-        h, w = image.shape[:2]
-        new_h, new_w = self.output_size
 
-        top, left = np.random.randint(0, h - new_h), np.random.randint(0, w - new_w)
-        image = image[top:top + new_h, left:left + new_w]
+class RandomColorJitter(object):
+    """ randomly jitter colour """
 
-        return {'image': image, 'label': label, 'image_id': id}
+    def __init__(self, brightness=0, hue=0, contrast=0, saturation=0):
+        self._brightness = brightness
+        self._hue = hue
+        self._contrast = contrast
+        self._saturation = saturation
+
+    def __call__(self, sample):
+        image, label, id = sample['image'], sample['label'], sample['image_id']
+        ColorJitter = transforms.ColorJitter(brightness=self._brightness, 
+                                             contrast=self._contrast,
+                                             saturation=self._saturation,
+                                             hue=self._hue)
+        img = ColorJitter(image)
+        return {'image': img, 'label': label, 'image_id': id}
+
+
+class RandomAffine(object):
+    """ randomly transform with affine transformation """
+
+    def __init__(self, degrees=5, translate=(0.05, 0.05)):
+        self._degrees = degrees
+        self._translate = translate
+
+    def __call__(self, sample):
+        image, label, id = sample['image'], sample['label'], sample['image_id']
+        Affine = transforms.RandomAffine(self._degrees, self._translate)
+        img = Affine(image)
+        return {'image': img, 'label': label, 'image_id': id}
+
+
+
+class RandomHorizontalFlip(object):
+    """ randomly apply horizontal flip to image """
+
+    def __init__(self, prob=0.5):
+        self._prob = prob
+
+    def __call__(self, sample):
+        image, label, id = sample['image'], sample['label'], sample['image_id']
+        Flip = transforms.RandomHorizontalFlip(self._prob)
+        img = Flip(image)
+        return {'image': img, 'label': label, 'image_id': id}
+
+
+class RandomPixelIntensityScaling(object):
+    """ randomly scale pixel intensities in an image """
+
+    def __init__(self, scale_min=0.75, scale_max=1.25):
+        self.scale_min = scale_min
+        self.scale_max = scale_max
+
+    def __call__(self, sample):
+        img = sample['image']
+        label = sample['label']
+        id = sample['image_id']
+
+        # TODO we have a PIL image
+        # if prob:
+        # do transform
+        # randomly choose a scale value between scale_min/max
+        # multiply entire image intensities by scale
+        # save to image sample/return image sample
+        # else
+        # just return normal
+        # if random.random() >= self.prob:
+            
+            
+            # random scale factor between the two min/max values
+        scale = random.uniform(self.scale_min, self.scale_max)
+        # scale image data
+        img = tvtransfunc.adjust_brightness(img, scale)
+        # img = np.array(img, np.float32) * float(scale)
+        # # fix min/max, since 
+        # img = Image.fromarray(img)
+        return {'image': img, 'label': label, 'image_id': id}
+
+
+# TODO rotation, then  scaled horz/vert, each colour channel shifted
+# can't we just call torchvision.transforms.RandomX?
+# class RandomHorizontalFlip(object):
+#     """ random horizontal flip """
+#     def __init__(self, prob):
+#         """ probability of horizontal image flip """
+#         self.prob = prob
+
+# class RandomRotate(object):
+#     """ randomly rotate image """
+#     def __init__(self, prob):
+#         self.prob = prob
+
+# class RandomColourJitter(object):
+#     """ randomly shift pixel intensity, colour"""
+#     # sere torchvision.transforms.ColorJitter(brightness)
+#     def __init__(self, prob):
+#         self.prob = prob
+
+# class RandomPerspective(object):
+#     """ randomly apply affine transformation for perspective shift """
+#     # see RandomAffine
+#     def ___init__(self, prob):
+#         self.prob = prob
+
 
 
 class ToTensor(object):
@@ -104,8 +239,31 @@ class ToTensor(object):
     def __call__(self, sample):
         image, label, id = sample['image'], sample['label'], sample['image_id']
 
-        image = image.transpose((2, 0, 1))
-        return {'image': torch.from_numpy(image), 'label': label, 'image_id': id}
+        # image = image.transpose((2, 0, 1))
+        # return {'image': torch.from_numpy(image), 'label': label, 'image_id': id}
+        image = tvtransfunc.to_tensor(image)
+        image = torch.as_tensor(image, dtype=torch.float32)
+
+        return {'image': image, 'label': label, 'image_id': id}
+
+
+class Compose(object):
+    """ Compose for set of transforms """
+
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+
+    def __call__(self, sample):
+        # NOTE this is done because the built-in PyTorch Compose transforms function
+        # only accepts a single (image/tensor) input. To operate on both the image
+        #  as well as the sample/target, we need a custom Compose transform function
+        for t in self.transforms:
+            sample= t(sample)
+        return sample
+
+
 
 ########### classes #############
 CLASSES = (0, 1, 2, 3, 4, 5, 6, 7, 8)
