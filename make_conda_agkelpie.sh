0b6a8c0fe965a99f844bd19a09bff3feec65293c
Dorian Tsai <dorian.tsai@gmail.com>
2022-10-12 09:48:01 +1000

    updated conda environment and build script with version numbers

--- make_conda_agkelpie.sh
+++ make_conda_agkelpie.sh
@@ -9,6 +9,8 @@ mamba env create -f agkelpie.yml
 # UPDATE: removed, as now redundant with "dependencies: opencv" in the agkelpie.yml file
 # pip install --no-binary opencv-python opencv-python
 
+conda activate agkelpie
+
 # for weed-camera interface, unsure if zeromq == zmq, stackoverflow seems
 # to imply they are different
 pip install zmq
