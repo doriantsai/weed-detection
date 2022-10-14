8ba8f0fb5638157ae44adab0bd5adc4e367324da
Dorian Tsai <dorian.tsai@gmail.com>
2022-10-14 14:56:05 +1000

    Detections tested and printing

--- weed_detection/Detections.py
+++ weed_detection/Detections.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python3
 
+import numpy as np
+
 from Region import Region
 
 class Detections(Region):
+
 
     SHAPE_POLY = 'polygon'
     SHAPE_RECT = 'rect'
@@ -18,12 +21,14 @@ class Detections(Region):
                  y = False, 
                  shape_type: str = False, 
                  centroid = False):
-
+        
         self.label = label
-        self.class_name = self.CLASS_NAMES[label]
+        # self.class_name = self.CLASS_NAMES[label]
+        Region.__init__(self, self.CLASS_NAMES[label], x, y)
+
         self.score = score
 
-        if shape_type not in [self.SHAPE_TYPES]:
+        if shape_type not in self.SHAPE_TYPES:
             raise TypeError('Unknown shape_type passed to Detections __init__()')
         else:
             self.shape_type = str(shape_type)
@@ -31,8 +36,8 @@ class Detections(Region):
         self.shape = self.make_shape(x, y)
         self.box = self.make_box(x, y) # [xmin, ymin, xmax, ymax]
         centroid = self.get_centroid()
-        self.centroid_x = centroid(0)
-        self.centroid_y = centroid(1)
+        self.centroid_x = centroid[0]
+        self.centroid_y = centroid[1]
          
 
     def get_centroid(self):
@@ -56,5 +61,30 @@ class Detections(Region):
         print(f'score: {self.score}')
         Region.print(self)
         print(f'centroid: ({self.centroid_x}, {self.centroid_y})')
+        print(f'Bounding box (xmin, ymin, xmax, ymax): {self.box}')
 
- 
+
+if __name__ == "__main__":
+
+    print('Detections.py')
+
+    """ testing input """
+
+    # test basic init for bbox
+    x = np.array([0, 0, 1, 1])
+    y = np.array([0, 1, 1, 0])
+    det = Detections(label = 1,
+                     score = 0.75,
+                     x = x,
+                     y = y,
+                     shape_type = 'polygon')
+    det.print()
+
+    # test polygon (triangle) with bbox:
+    x = np.array([0, 3, 6])
+    y = np.array([0, 3, 0])
+    det_tri = Detections(0, 0.5, x, y, 'polygon')
+    det_tri.print()
+    # python debug code
+    # import code
+    # code.interact(local=dict(globals(), **locals()))
