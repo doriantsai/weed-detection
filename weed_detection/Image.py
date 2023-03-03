7e69a2986b1730ce83b961e26bdc24a1249f137d
Dorian Tsai <dorian.tsai@gmail.com>
2023-03-03 13:46:54 +1000

    removed filesize, and unused attributes

--- weed_detection/Image.py
+++ weed_detection/Image.py
@@ -9,14 +9,12 @@ from Region import Region
 class Image:
 
     def __init__(self, 
-                 filename: str, 
-                 filesize: int,
-                 file_attr: dict,
-                 width: int,
-                 height: int, 
-                 camera: str):
+                 filename: str,
+                 file_attr: dict = None,
+                 width: int = None,
+                 height: int = None, 
+                 camera: str = None):
         self.filename = filename
-        self.filesize = filesize
         self.file_attr = file_attr
         self.width = width
         self.height = height
@@ -25,7 +23,7 @@ class Image:
 
     def print(self):
         print('Filename: ' + self.filename)
-        print(f'Filesize: {self.filesize}')
+        # print(f'Filesize: {self.filesize}')
         print('File attributes:')
         print(self.file_attr)
         print(f'Image (width, height): ({self.width, self.height}) pix')
